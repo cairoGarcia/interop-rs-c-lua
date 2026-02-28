1ef41255c6c86a4855cced23faf93cbc31bb2231
cairoGarcia <bromacaga@gmail.com>
2026-02-28 18:03:46 -0300

    first version, no lua interop yet

---
+++ c/lib_c.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// rust safe function
+char *get_string();
+
+// rust safe function
+void square(int size);
+
+// rust safe function w/ unsafe block
+void writeout(char *s);
---
+++ c/main.c
@@ -0,0 +1,9 @@
+#include "lib_c.h"
+
+int main()
+{
+    // test function calls, don't wind the hardcoded shit
+    writeout(get_string());
+    square(4);
+    return 0;
+}
